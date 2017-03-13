4c54d9902899fd64172f89c1a878f89a30c37bd7
Yoonha Kang <yoonhakang@10-4-234-254.vlan890.client.wireless.uic.edu>
2017-03-13 15:37:01 -0500

    yoonha

---
+++ 342Final/src/frame.java
@@ -0,0 +1,33 @@
+import javax.swing.JFrame;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+
+class frame extends JFrame{
+	public frame(){
+		this("Restaurant Portal");
+	}
+
+
+	public frame(String title) {
+		this.setSize(500,400);
+		this.setTitle(title);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		FlowLayout layout = new FlowLayout();
+		this.setLayout(layout);
+		
+		JButton button1 = new JButton("Customer");
+		JButton button2 = new JButton("Restaurnat Admin");
+		JButton button3 = new JButton("New Customer");
+		
+		this.add(button1);
+		this.add(button2);
+		this.add(button3);
+		
+		this.setVisible(true);
+	}
+
+}
+
